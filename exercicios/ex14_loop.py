2c251ad18b24e81f6da3d1ae4a5e8c610dd228ae
sofiapetruk <sofiapetruk900@gmail.com>
2024-04-08 13:57:17 -0300

    alterações

---
+++ exercicios/ex14_loop.py
@@ -0,0 +1,12 @@
+#Contagem regressiva:
+#Escreva um programa que solicita ao usuário um número inteiro positivo e, em seguida, 
+#conta regressivamente desse número até zero, imprimindo cada número no processo.
+
+#entrada
+numero = int(input("DIgite um número: "))
+contagem = 0
+#processamento
+while numero >= contagem:
+    print(numero)
+    numero -= 1
+
